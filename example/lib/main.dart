8b6d27c81b249032c06ca9f1d0921870fa31c2aa
Prateek SU <pankajsunal123@gmail.com>
2022-04-09 00:15:50 +0530

    Update files [skip ci]

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,12 +18,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
